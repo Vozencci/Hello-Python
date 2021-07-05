40960c7b44ff99821c8d2d8b96a7c79bc3f105f1
Vozencci <86961350+Vozencci@users.noreply.github.com>
2021-07-05 22:16:47 +0200

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -1,3 +1,12 @@
+# Disclaimer: Ce code a été tapé par Vozencci aka Sarmeegan.
+# Tout droit réservé, code Open Source.
+
+# Ce code est une traduction en python de comment ecrire un nombre en base 10 en base 2 (la conversion binaire).
+# Idée: Faire une succession de division euclidienne par 2 du nombre n choisi, puis lorsque la division euclidienne donne le chiffre 1 comme quotient: diviser une derniere fois ce quotient pour obtenir 1 comme reste. Finalement, on colle les reste ensembles en partant du reste situé vers la dernière division. (Exemple: 100101010)
+# Amusez-vous bien !
+
+
+
 def Binaire(n):
     b=str()
     while not(n//2==1):
